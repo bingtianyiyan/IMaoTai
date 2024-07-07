16b0aeac52b09e2cc3c173b9e2f28034deceda35
ydy <517697206@qq.com>
2024-07-07 11:18:04 +0800

    feat:productList

---
+++ IMaoTai/Service/AppointProjectService.cs
@@ -0,0 +1,22 @@
+﻿using IMaoTai.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IMaoTai.Service
+{
+    public class AppointProjectService:IAppointProjectService
+    {
+
+       public async Task RefreshProduct()
+        {
+            App.MtSessionId = string.Empty;
+            App.WriteCache("mtSessionId.txt", string.Empty);
+            AppointProjectViewModel.ProductList.Clear();
+            await IMTService.GetCurrentSessionId();
+        }
+    }
+}
